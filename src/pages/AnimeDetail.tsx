d2408be20d9e8235835ac7ddd64c1e5c7847dd05
keyaru77 <whyudacok@gmail.com>
2024-07-06 08:33:42 +0700

    Update AnimeDetail.tsx

--- src/pages/AnimeDetail.tsx
+++ src/pages/AnimeDetail.tsx
@@ -1,17 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import PostCard from "../components/PostCard";
 
-const AnimeDetail: React.FC = () => {
-  const { end } = useParams<{ end: string }>();
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
-  // Lakukan hal lain sesuai kebutuhan dengan nilai 'end' yang didapat dari useParams
+const Post: React.FC = () => {
+  const [post, setPost] = useState<Post>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((response) => response.json())
+      .then((json) => setPost(json));
+  }, []);
 
   return (
-    <div>
-      <h2>Detail untuk {end}</h2>
-      {/* Tampilkan detail anime atau konten sesuai dengan nilai 'end' */}
+    <div className="flex items-center justify-center h-screen">
+      <PostCard id={post?.id} title={post?.title} body={post?.body} />
     </div>
   );
 };
 
-export default AnimeDetail;
+export default Post;
