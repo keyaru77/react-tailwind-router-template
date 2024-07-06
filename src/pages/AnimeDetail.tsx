f369680d99ddc5b2a7c5c82eff4649345ea17ffa
keyaru77 <whyudacok@gmail.com>
2024-07-06 08:23:19 +0700

    Update AnimeDetail.tsx

--- src/pages/AnimeDetail.tsx
+++ src/pages/AnimeDetail.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import AnimeCard from "../components/AnimeCard";
 
 interface Episode {
   href: string;
