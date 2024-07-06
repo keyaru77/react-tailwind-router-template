c8adebf7016d9c945457f773ecc647fced532854
keyaru77 <whyudacok@gmail.com>
2024-07-06 07:24:14 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Post from "./pages/post";
-import AboutUs from "./pages/aboutUs";
 import NotFound from "./pages/notFound";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
-        <Route path="/aboutus" element={<AboutUs />} />
         <Route path="*" element={<NotFound />} />
         {/* Add more routes as needed */}
       </Routes>
