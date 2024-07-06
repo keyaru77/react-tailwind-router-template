3bba0a1a981e2ac22ce48088f1166e745347d700
keyaru77 <whyudacok@gmail.com>
2024-07-06 07:40:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Post from './pages/post';
-import NotFound from './pages/notFound';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Post from "./pages/post";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
@@ -11,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
         <Route path="*" element={<NotFound />} />
+        {/* Add more routes as needed */}
       </Routes>
     </>
   );
